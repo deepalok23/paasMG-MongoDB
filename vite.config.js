7385fa2f7d66d4ad785c811b1bd90f4d30b38365
ALOKSINGH A <alok63210@gmail.com>
2026-02-21 03:30:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // From our previous fix
 
 export default defineConfig({
+  base: '/paasMG-MongoDB/',
   plugins: [
     react(),
     tailwindcss(),
